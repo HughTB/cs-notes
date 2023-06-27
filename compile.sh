7478af1131758136f5f26749eca4bb00e0737d0f
HughTB <hugh.baldwin77@gmail.com>
2023-06-27 18:28:46 +0100

    add compilation script and 2nd year module templates

---
+++ compile.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+ROOT_DIR=$PWD
+TEX_ENGINE=xelatex
+
+cd $ROOT_DIR/2nd-year/m21270-dsalg/
+echo "Compiling m21270-dsalg"
+$TEX_ENGINE m21270.tex
+
+cd $ROOT_DIR/2nd-year/m21274-dmafp/
+echo "Compiling m21274-dmafp"
+$TEX_ENGINE m21274.tex
+
+cd $ROOT_DIR/2nd-year/m30233-osint/
+echo "Compiling m30233-osint"
+$TEX_ENGINE m30233.tex
+
+cd $ROOT_DIR/2nd-year/m30235-paapl/
+echo "Compiling m30235-paapl"
+$TEX_ENGINE m30235.tex
+
+cd $ROOT_DIR/2nd-year/m30239-ehack/
+echo "Compiling m30239-ehack"
+$TEX_ENGINE m30239.tex
+
+cd $ROOT_DIR/2nd-year/m30819-setap/
+echo "Compiling m30819-setap"
+$TEX_ENGINE m30819.tex
+
+cd $ROOT_DIR
