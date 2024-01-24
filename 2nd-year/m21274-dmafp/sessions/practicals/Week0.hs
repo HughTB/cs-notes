eabb03549c19e458abc76d279586dd2e29bf0eea
HughTB <hugh.baldwin77@gmail.com>
2024-01-24 11:46:43 +0000

    add 1st fp practical

---
+++ 2nd-year/m21274-dmafp/sessions/practicals/Week0.hs
@@ -0,0 +1,8 @@
+square :: Int -> Int
+square x = x * x
+
+mult2 :: Int -> Int
+mult2 x = 2 * x
+
+mult4 :: Int -> Int
+mult4 x = mult2 (mult2 x)
---
+++ 2nd-year/m21274-dmafp/sessions/practicals/Week1.hs
@@ -0,0 +1,39 @@
+-- Ex 1
+timesTen :: Int -> Int
+timesTen x = x * 10
+
+-- Ex 2
+sumThree :: Int -> Int -> Int -> Int
+sumThree x y z = x + y + z
+
+-- Ex 3
+areaOfCircle :: Float -> Float
+areaOfCircle r = pi * r^2
+
+-- Ex 4
+volumeOfCylinder :: Float -> Float -> Float
+volumeOfCylinder h r = pi * r^2 * h
+
+-- Ex 5
+distance :: Float -> Float -> Float -> Float -> Float
+distance x1 y1 x2 y2 = sqrt((y1 - y2)^2 + (x1 - x2)^2)
+
+-- Ex 6
+threeDifferent :: Int -> Int -> Int -> Bool
+threeDifferent x y z = x /= y && y /= z && x /= z
+
+-- Ex 7
+divisibleBy :: Int -> Int -> Bool
+divisibleBy x y = (mod x y) == 0
+
+-- Ex 8
+isEven :: Int -> Bool
+isEven x = divisibleBy x 2
+
+-- Ex 9
+averageThree :: Int -> Int -> Int -> Float
+averageThree x y z = fromIntegral (x + y + z) / 3
+
+-- Ex 10
+absolute :: Int -> Int
+absolute x = if x > 0 then x else x * (-1)
